463d62ab977eb13f00a60378a8198f378ca2c804
Darguen <pvidela17@alumnos.utalca.cl>
2023-09-11 09:16:22 -0300

    second activitiy

--- app/src/main/java/pvidela/MyBopIt/MainActivity.kt
+++ app/src/main/java/pvidela/MyBopIt/MainActivity.kt
@@ -3,15 +3,27 @@ package pvidela.MyBopIt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_about)
+
+        /*val buttonOKAbout = findViewById<Button>(R.id.mainButton)
+
+        // Button click listeners
+        buttonOKAbout.setOnClickListener {
+            val intentAbout = Intent(this, AboutActivity::class.java)
+            startActivity(intentAbout)
+        }*/
     }
 
     //val button:
 
     //val intentAbout = Intent(this, AboutActivity::class.java)
     //startActivity(intentAbout)
+
+
+
 }
